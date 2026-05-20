f1e8c92ade68a6bde7e3784f4217228318d4913b
Rabindra Dhakal <contact@qaidvoid.dev>
2026-05-20 22:09:24 +0545

    fix: correct webp fuzz target enum spelling

--- fuzz/fuzz_targets/webp_decode.rs
+++ fuzz/fuzz_targets/webp_decode.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     let _ = glycin_ng::Loader::new_bytes(data.to_vec())
         .sandbox_selector(glycin_ng::SandboxSelector::none())
-        .format_hint(glycin_ng::KnownFormat::Webp)
+        .format_hint(glycin_ng::KnownFormat::WebP)
         .load();
 });
